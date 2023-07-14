138b3d802825e42d9153387e1739b0e8e60a0901
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-15 00:44:01 +0300

    Create filtersSlice.js

---
+++ src/redux/filtersSlice.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+    filters: ''
+};
+
+export const filtersReducer = (state = initialState,  action) => {
+    switch (action.type) {
+        case '':
+            
+            break;
+    
+        default:
+            break;
+    }
+}
