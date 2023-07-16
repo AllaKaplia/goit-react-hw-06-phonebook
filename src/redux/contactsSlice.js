0491cd4fb010c08294cdbaa57dd77b80a9aed436
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-16 21:20:47 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,28 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 import contactsDefault from './contastsDefault.json';
+import { v4 as uuidv4 } from 'uuid';
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
+
 
 const initialState = {
-  contacts: JSON.parse(window.localStorage.getItem('KEY')) ?? contactsDefault,
-  filter: '',
+    contacts: contactsDefault,
+    filter: '',
 };
-
+  
 const contactsSlice = createSlice({
-  name: 'contacts',
-  initialState,
-  reducers: {
-    addContact: (state, action) => {
-      state.contacts.push(action.payload);
+    name: 'contacts',
+    initialState,
+    reducers: {
+      addContact: (state, action) => {
+        const newContact = { ...action.payload, id: uuidv4() };
+        state.contacts.push(newContact);
+      },
+      deleteContact: (state, action) => {
+        state.contacts = state.contacts.filter(
+          contact => contact.name !== action.payload
+        );
+      },
+      changeFilter: (state, action) => {
+        state.filter = action.payload;
+      },
     },
-    deleteContact: (state, action) => {
-      state.contacts = state.contacts.filter(
-        (contact) => contact.name !== action.payload
-      );
-    },
-    changeFilter: (state, action) => {
-      state.filter = action.payload;
-    },
-  },
 });
+  
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['contacts']
+}
+  
+export const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
+  
 export const { addContact, deleteContact, changeFilter } = contactsSlice.actions;
-export default contactsSlice.reducer;
