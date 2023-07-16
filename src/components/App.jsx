89a9fcfd3e8c7edf187c29059e46f20f283de207
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-16 21:20:40 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addContact, deleteContact, changeFilter } from "../redux/contactsSlice";
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from '../redux/store';
 import ContactList from "./ContactList";
 import FilterContacts from "./FilterContacts";
 import FormContact from "./FormContact";
 import { toast, ToastContainer } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
 import "react-toastify/dist/ReactToastify.css";
 import { Container } from './App.styled';
+
 
 export default function App() {
   const dispatch = useDispatch();
@@ -32,7 +36,8 @@ export default function App() {
       return;
     }
 
-    dispatch(addContact(newContact));
+    const contactWithId = { ...newContact, id: uuidv4() };
+    dispatch(addContact(contactWithId));
   };
 
   const deleteContactHandler = (contactName) => {
@@ -54,12 +59,14 @@ export default function App() {
 
   return (
     <Container>
-      <h1>Phonebook</h1>
-      <FormContact addContact={formSubmitHandle} />
-      <h2>Contacts</h2>
-      <FilterContacts value={filter} onChange={changeFilterHandler} />
-      <ContactList contacts={visibleContacts} onDeleteContact={deleteContactHandler} />
-      <ToastContainer theme="colored" />
+      <PersistGate loading={null} persistor={persistor}>
+        <h1>Phonebook</h1>
+        <FormContact addContact={formSubmitHandle} />
+        <h2>Contacts</h2>
+        <FilterContacts value={filter} onChange={changeFilterHandler} />
+        <ContactList contacts={visibleContacts} onDeleteContact={deleteContactHandler} />
+        <ToastContainer theme="colored" />
+      </PersistGate>
     </Container>
   );
 }
