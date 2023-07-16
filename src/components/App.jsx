68c47ad0a70fd769cdd914ed6d3a7d48694c2db9
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-16 17:38:53 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import contactsDefault from './contactsDefault.json'; 
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addContact, deleteContact, changeFilter } from "../redux/contactsSlice";
 import ContactList from "./ContactList";
 import FilterContacts from "./FilterContacts";
 import FormContact from "./FormContact";
@@ -8,68 +8,58 @@ import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { Container } from './App.styled';
 
-
-export default function App(){
+export default function App() {
   const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.contacts);
+  const filter = useSelector(state => state.contacts.filter);
 
-  const [contacts, setContacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem('KEY')) ?? contactsDefault
-  });
-  const [filter, setFilter] = useState('');
-  const KEY = contacts;
-
-  useEffect(() => {
-    window.localStorage.setItem('KEY', JSON.stringify(KEY));
-  })
-
-  const formSubmitHandle = newContact => {
-  
+  const formSubmitHandle = (newContact) => {
     const existingContactByName = contacts.find(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
-  
+
     const existingContactByNumber = contacts.find(
       contact => contact.number === newContact.number
     );
-  
+
     if (existingContactByName) {
       toast.warn('A contact with this name already exists!');
       return;
     }
-  
+
     if (existingContactByNumber) {
       toast.info('A contact with this number already exists!');
       return;
     }
-  
-    addContact(newContact);
-  }
 
-  const addContact = (contact) => {
-    setContacts(prevState => [...prevState, contact]);
-  }
+    dispatch(addContact(newContact));
+  };
 
-  const deleteContact = (contactName) => {
-    setContacts(prevState => prevState.filter(contact => contact.name !== contactName));
-  }
+  const deleteContactHandler = (contactName) => {
+    dispatch(deleteContact(contactName));
+  };
 
-  const changeFilter = evt => {
-    setFilter(evt.currentTarget.value);
-  }
+  const changeFilterHandler = (evt) => {
+    dispatch(changeFilter(evt.currentTarget.value));
+  };
 
   const normalizedFilter = filter ? filter.toLowerCase() : '';
   const visibleContacts = contacts.filter(contact =>
     contact.name.toLowerCase().includes(normalizedFilter)
   );
 
+  useEffect(() => {
+    window.localStorage.setItem('KEY', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <Container>
       <h1>Phonebook</h1>
-      <FormContact addContact={formSubmitHandle}/>
+      <FormContact addContact={formSubmitHandle} />
       <h2>Contacts</h2>
-      <FilterContacts value={filter} onChange={changeFilter} />
-      <ContactList contacts={visibleContacts} onDeleteContact={deleteContact} />
-      <ToastContainer theme="colored"/>
+      <FilterContacts value={filter} onChange={changeFilterHandler} />
+      <ContactList contacts={visibleContacts} onDeleteContact={deleteContactHandler} />
+      <ToastContainer theme="colored" />
     </Container>
   );
 }
