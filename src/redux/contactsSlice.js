33b7da2b0977d6e931696e060a5626c506350f63
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-16 17:39:03 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,54 +1,28 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+import contactsDefault from './contastsDefault.json';
 
 const initialState = {
-    contasts: [
-        {
-            "id": "id-1", 
-            "name": "Rosie Simpson", 
-            "number": "459-12-56"
-        },
-    
-        {
-            "id": "id-2",
-            "name": "Hermione Kline",
-            "number": "443-89-12"
-        },
-    
-        {
-            "id": "id-3",
-            "name": "Eden Clements",
-            "number": "645-17-79"
-        },
-    
-        {
-            "id": "id-4",
-            "name": "Annie Copeland",
-            "number": "227-91-26"
-        },
-    
-        {
-            "id": "id-5",
-            "name": "Dominika Sova",
-            "number":"227-91-28"
-        }
-    ]
-}
+  contacts: JSON.parse(window.localStorage.getItem('KEY')) ?? contactsDefault,
+  filter: '',
+};
 
-export const addContact = createAction('contacts/addContact');
-
-export const removeContact = createAction('contacts/removeContact');
-
-export const contactsReducer = createReducer(initialState, {
-    [addContact](state, action) {
-        return {
-            ...state,
-            contacts: action.payload,
-        };
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts.push(action.payload);
     },
-    [removeContact](state, action) {
-        return {
-            ...state,
-            contacts: state.filter(contact => contact.id !== action.payload),
-        };
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        (contact) => contact.name !== action.payload
+      );
     },
+    changeFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
 });
+
+export const { addContact, deleteContact, changeFilter } = contactsSlice.actions;
+export default contactsSlice.reducer;
