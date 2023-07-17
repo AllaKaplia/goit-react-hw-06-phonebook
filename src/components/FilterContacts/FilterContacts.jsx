0c88cf7938ebcf51299e56379fc5882927786714
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-17 14:04:50 +0300

    Update FilterContacts.jsx

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { nanoid } from 'nanoid';
 import { LabelFilter, InputFilter } from "./FilterContacts.styled";
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter } from '../../redux/contactsSlice';
 
-const FilterContacts = ({ value, onChange }) => {
+const FilterContacts = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(state => state.contacts.filter);
+
+  const onChange = (event) => {
+    const newFilterValue = event.target.value;
+    dispatch(changeFilter(newFilterValue));
+  };
+
   return (
     <LabelFilter>
       Filter
       <InputFilter
         type="text"
-        value={value}
+        value={filter}
         id={nanoid()}
         name="filter"
         onChange={onChange} 
