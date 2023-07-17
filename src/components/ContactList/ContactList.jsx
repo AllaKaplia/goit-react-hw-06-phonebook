ad348bb9dca94ac29872e1c14c767f562ea3d228
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-17 14:04:48 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,28 @@
 import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
 import { ContactsList, ContactItem, RemoveBtn } from './ContactsList.styled';
+import { deleteContact, selectVisibleContacts } from '../../redux/contactsSlice';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
-    return (
-        <ContactsList>
-        {contacts.map(({ name, number }) => (
-          <ContactItem key={nanoid()}>
-            {name}: {number}
-            <RemoveBtn type='button' onClick={() => onDeleteContact(name)}>
-              Remove
-            </RemoveBtn>
-          </ContactItem>
-        ))}
-      </ContactsList>
-    );
+const ContactList = () => {
+  const dispatch = useDispatch();
+  const visibleContacts = useSelector(selectVisibleContacts);
+
+  const handleDelete = (name) => {
+    dispatch(deleteContact(name));
+  };
+
+  return (
+    <ContactsList>
+      {visibleContacts.map(({ name, number }) => (
+      <ContactItem key={nanoid()}>
+      {name}: {number}
+        <RemoveBtn type='button' onClick={() => handleDelete(name)}>
+          Remove
+        </RemoveBtn>
+      </ContactItem>
+    ))}
+    </ContactsList>
+  );
 }
 
 export default ContactList;
