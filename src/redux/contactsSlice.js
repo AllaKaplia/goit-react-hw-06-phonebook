8134d073e038e1a122c9b4e336ac963309c46a1f
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-17 14:05:01 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,41 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSelector, createSlice } from '@reduxjs/toolkit';
 import contactsDefault from './contastsDefault.json';
-import { nanoid } from 'nanoid';
-import { persistReducer } from 'redux-persist'
+import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-
 const initialState = {
-    contacts: contactsDefault,
-    filter: '',
+  contacts: contactsDefault,
+  filter: '',
 };
-  
+
 const contactsSlice = createSlice({
-    name: 'contacts',
-    initialState,
-    reducers: {
-      addContact: (state, action) => {
-        const newContact = { ...action.payload, id: nanoid() };
-        state.contacts.push(newContact);
-      },
-      deleteContact: (state, action) => {
-        state.contacts = state.contacts.filter(
-          contact => contact.name !== action.payload
-        );
-      },
-      changeFilter: (state, action) => {
-        state.filter = action.payload;
-      },
+  name: 'contacts',
+  initialState,
+  reducers: {
+    addContact: (state, action) => {
+      state.contacts = [...state.contacts, action.payload];
     },
+    deleteContact: (state, action) => {
+      state.contacts = state.contacts.filter(
+        (contact) => contact.name !== action.payload
+      );
+    },
+    changeFilter: (state, action) => {
+      state.filter = action.payload;
+    },
+  },
 });
-  
 
 const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['contacts']
-}
-  
+  key: 'root',
+  storage,
+  whitelist: ['contacts'],
+};
+
 export const contactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
-  
+
 export const { addContact, deleteContact, changeFilter } = contactsSlice.actions;
+
+export const selectVisibleContacts = createSelector(
+  (state) => state.contacts.filter,
+  (state) => state.contacts.contacts,
+  (filter, contacts) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
