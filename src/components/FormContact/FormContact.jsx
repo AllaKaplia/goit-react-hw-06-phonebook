88dd94fc57920ed7b3ba3764d651d7e1f9b83b0a
Alla Kaplia <kaplia.alla18@gmail.com>
2023-07-17 14:04:54 +0300

    Update FormContact.jsx

--- src/components/FormContact/FormContact.jsx
+++ src/components/FormContact/FormContact.jsx
@@ -1,14 +1,11 @@
 import * as yup from 'yup';
 import { Formik, Form, ErrorMessage } from 'formik';
-import { nanoid } from 'nanoid';
-import { Input, ButtonAdd, LabelForm, ErrorText } from "./FormContact.styled";
-
-const ErrorError = ({ name }) => {
-  return (
-    <ErrorMessage name={name}
-    render={message => <ErrorText>{message}</ErrorText>} />
-  );
-};
+import { Input, ButtonAdd, LabelForm, ErrorText } from './FormContact.styled';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
+import { addContact } from '../../redux/contactsSlice';
+import { v4 as uuidv4 } from 'uuid';
 
 const schema = yup.object().shape({
   name: yup
@@ -23,44 +20,78 @@ const schema = yup.object().shape({
 
 const initialValue = {
   name: '',
-  number: ''
-}
+  number: '',
+};
 
-const FormContact = ({ addContact }) => {
-  const handleSubmit = (values, { resetForm }) => {
-    addContact(values);
+const ErrorError = ({ name }) => {
+  return (
+    <ErrorMessage
+      name={name}
+      render={message => <ErrorText>{message}</ErrorText>}
+    />
+  );
+};
+
+const FormContact = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector((state) => state.contacts.contacts);
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleFormSubmit = (newContact, { resetForm }) => {
+    const existingContactByName = contacts.find(
+      (contact) => contact.name.toLowerCase() === newContact.name.toLowerCase()
+    );
+
+    const existingContactByNumber = contacts.find(
+      (contact) => contact.number === newContact.number
+    );
+
+    if (existingContactByName) {
+      toast.warn('A contact with this name already exists!');
+      return;
+    }
+
+    if (existingContactByNumber) {
+      toast.info('A contact with this number already exists!');
+      return;
+    }
+
+    const contactWithId = { ...newContact, id: uuidv4() };
+    dispatch(addContact(contactWithId));
+
     resetForm();
-  }
+  };
 
   return (
-    <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleSubmit}>
+    <Formik initialValues={initialValue} validationSchema={schema} onSubmit={handleFormSubmit}>
       <Form autoComplete='off'>
-      <LabelForm>
-        The name of the new contact
-        <Input
-          type="text"
-          name="name"
-          id={nanoid()}
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <ErrorError name='name' />
-      </LabelForm>
+        <LabelForm>
+          The name of the new contact
+          <Input
+            type='text'
+            name='name'
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <ErrorError name='name' />
+        </LabelForm>
         <LabelForm>
           Phone number
           <Input
-            type="tel"
-            name="number"
-            id={nanoid()}
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            type='tel'
+            name='number'
+            title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
             required
           />
-          <ErrorError name='number' />
+          <ErrorError name='message' />
         </LabelForm>
-        <ButtonAdd type="submit">Add contact</ButtonAdd>
+        <ButtonAdd type='submit'>Add contact</ButtonAdd>
       </Form>
     </Formik>
   );
-}
+};
 
 export default FormContact;
